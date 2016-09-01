47bc84cb795ed634496a71d79126cb493ac0b4ce
Paul LaCrosse <paul.lacrosse@gmail.com>
2016-09-01 16:12:49 -0400

    check test results for pass

--- package.sh
+++ package.sh
@@ -3,15 +3,21 @@ export app=agildata-zero
 export exe=target/release/$app
 
 cargo test
-cargo clean
-cargo build --release
-rc=$?; if [[ $rc == 0 ]]; then
-    echo $app build completed
-    rm -Rf dist
-    mkdir dist
-    cp ./doc/README.md dist/
-    cp example-zero-config.xml dist/
-    cp $exe dist/
-    tar -vczf $app.tar.gz -C dist .
-    echo $app.tar.gz packaging completed
+rc=$?; if [[ $rc != 0 ]]; then
+    exit
 fi
+echo $app tests completed
+cargo clean
+echo $app clean build starting
+cargo build --release
+rc=$?; if [[ $rc != 0 ]]; then
+    exit
+fi
+echo $app release build completed
+rm -Rf dist
+mkdir dist
+cp ./doc/README.md dist/
+cp example-zero-config.xml dist/
+cp $exe dist/
+tar -vczf $app.tar.gz -C dist .
+echo $app.tar.gz packaging completed
