615bfa0a0389b07ffc00343367b516b5bd4063d7
Kenji Hollis <kenji.hollis@codefutures.com>
2016-09-01 15:41:41 -0600

    Updated test code to use sudo -t.

--- scripts/jenkins/test.sh
+++ scripts/jenkins/test.sh
@@ -2,11 +2,11 @@
 #
 # Script for testing AgilData Zero with Jenkins
 
-sudo service mysql start
+sudo -t service mysql start
 
 # Drop test databases
 # Copy test database info into MySQL
 # Query MySQL and run diffs against the values
 # If any diffs occur, indicate a build error, and report back the differences
 
-sudo service mysql stop
+sudo -t service mysql stop
