83695525f30af2b493e5102383794d75209cd89c
Paul LaCrosse <paul.lacrosse@gmail.com>
2016-09-02 09:39:39 -0400

    add version number to packaged tar file name

--- package.sh
+++ package.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export app=agildata-zero
 export exe=target/release/$app
+export ver=$(grep version Cargo.toml | cut -d'"' -f2)
+export out=$app-$ver.tar.gz
 
 rustup override set nightly-2016-08-31
 
@@ -17,10 +19,11 @@ rc=$?; if [[ $rc != 0 ]]; then
     exit 1
 fi
 echo $app release build completed
+rm $out
 rm -Rf dist
 mkdir dist
 cp ./doc/README.md dist/
 cp zero-config.xml dist/
 cp $exe dist/
-tar -vczf $app.tar.gz -C dist .
+tar -vczf $out -C dist .
 echo $app.tar.gz packaging completed
