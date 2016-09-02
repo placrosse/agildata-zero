4a7dfe6164f7b05c231b2d59442ca6a76c6e4862
Kenji Hollis <kenji.hollis@codefutures.com>
2016-09-02 11:25:36 -0600

    Updated test script.

--- scripts/jenkins/test.sh
+++ scripts/jenkins/test.sh
@@ -2,11 +2,9 @@
 #
 # Script for testing AgilData Zero with Jenkins
 
-sudo -t service mysql start
-
+# Build Zero
+# Start server in the background, storing the PID of the app
 # Drop test databases
 # Copy test database info into MySQL
 # Query MySQL and run diffs against the values
 # If any diffs occur, indicate a build error, and report back the differences
-
-sudo -t service mysql stop
