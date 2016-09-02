3431524e87aef22a743998e9072eb57a688cd506
Kenji Hollis <kenji.hollis@codefutures.com>
2016-09-02 12:48:42 -0600

    Updated to use zero database name.

--- scripts/jenkins/test.sh
+++ scripts/jenkins/test.sh
@@ -6,7 +6,8 @@
 # each step of the way as the build runs.
 
 RUST_BUILD="nightly-2016-08-03"
-AGILDATA_TEST_DB="itest$BUILD_NUMBER"
+# AGILDATA_TEST_DB="itest$BUILD_NUMBER"
+AGILDATA_TEST_DB="zero"
 
 MYSQL_USER="agiluser"
 MYSQL_PASS="password123"
@@ -38,7 +39,7 @@ ps -aux | grep $AGILDATA_ZERO_PID | grep -v grep
 
 # Create Database
 echo "Creating database: $AGILDATA_TEST_DB"
-mysql --host=127.0.0.1 --port=3306 -u$MYSQL_USER -p$MYSQL_PASS -e "CREATE DATABASE $AGILDATA_TEST_DB CHARACTER SET UTF8"
+mysql --host=127.0.0.1 --port=3307 -u$MYSQL_USER -p$MYSQL_PASS -e "CREATE DATABASE $AGILDATA_TEST_DB CHARACTER SET UTF8"
 
 # Copy test database info into MySQL
 mysql --host=127.0.0.1 --port=3307 -u$MYSQL_USER -p$MYSQL_PASS -D $AGILDATA_TEST_DB < scripts/test/test1.sql
@@ -48,7 +49,7 @@ mysql --host=127.0.0.1 --port=3307 -u$MYSQL_USER -p$MYSQL_PASS -D $AGILDATA_TEST
 
 # Drop Database
 echo "Dropping database: $AGILDATA_TEST_DB"
-mysql --host=127.0.0.1 --port=3306 -u$MYSQL_USER -p$MYSQL_PASS -e "DROP DATABASE $AGILDATA_TEST_DB"
+mysql --host=127.0.0.1 --port=3307 -u$MYSQL_USER -p$MYSQL_PASS -e "DROP DATABASE $AGILDATA_TEST_DB"
 
 # Stop AgilData Zero
 echo "Stopping AgilData Zero: $AGILDATA_ZERO_PID"
