67dda766e1ab2efe60eb0431dc2a3353209d1004
Kenji Hollis <kenji.hollis@codefutures.com>
2016-09-02 11:38:16 -0600

    Updated to use nightly build.

--- scripts/jenkins/test.sh
+++ scripts/jenkins/test.sh
@@ -2,9 +2,22 @@
 #
 # Script for testing AgilData Zero with Jenkins
 
+# Set the version of Rust to use
+rustup override set nightly-2016-08-03
+
 # Build Zero
+cargo build
+
 # Start server in the background, storing the PID of the app
+target/debug/agildata-zero &
+AGILDATA_ZERO_PID=$!
+
 # Drop test databases
+
 # Copy test database info into MySQL
 # Query MySQL and run diffs against the values
 # If any diffs occur, indicate a build error, and report back the differences
+
+# Stop AgilData Zero
+echo "Stopping AgilData Zero: $AGILDATA_ZERO_PID"
+kill $AGILDATA_ZERO_PID
