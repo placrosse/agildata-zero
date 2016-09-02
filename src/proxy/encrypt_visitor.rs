a5fb0d3ae2fdea72d7831e880eb3c90bf17adaa8
Drew Manlove <andrew.manlove@codefutures.com>
2016-09-02 12:43:02 -0600

    implement discriminate encryption for binary exprs in new visitor

--- src/proxy/encrypt_visitor.rs
+++ src/proxy/encrypt_visitor.rs
@@ -1,7 +1,8 @@
 use query::planner::{Rel, Rex, RelVisitor, TupleType, HasTupleType};
-use query::Operator;
+use query::{Operator, LiteralExpr};
 use std::collections::HashMap;
 use std::error::Error;
+use encrypt::*;
 
 pub struct EncryptVisitor {
 	pub valuemap: HashMap<u32, Result<Vec<u8>, Box<Error>>>
@@ -11,15 +12,15 @@ impl RelVisitor for EncryptVisitor  {
 	fn visit_rel(&mut self, rel: &Rel) -> Result<(), String> {
 		match rel {
 			&Rel::Projection{box ref project, box ref input, ref tt} => {
-				self.visit_rex(project, tt);
-				self.visit_rel(input);
+				self.visit_rex(project, tt)?;
+				self.visit_rel(input)?;
 			},
 			&Rel::Selection{box ref expr, box ref input} => {
-				self.visit_rex(expr, input.tt());
-				self.visit_rel(input);
+				self.visit_rex(expr, input.tt())?;
+				self.visit_rel(input)?;
 			},
-			&Rel::TableScan{ref table, ref tt} => {},
-			&Rel::Dual{ref tt} => {}
+			&Rel::TableScan{..} => {},
+			&Rel::Dual{..} => {}
 		}
 		Ok(())
 	}
@@ -29,19 +30,40 @@ impl RelVisitor for EncryptVisitor  {
 			&Rex::BinaryExpr{box ref left, ref op, box ref right} => {
 				match op {
 					&Operator::AND | &Operator::OR => {
-						self.visit_rex(left, tt);
-						self.visit_rex(right, tt);
+						self.visit_rex(left, tt)?;
+						self.visit_rex(right, tt)?;
 					}
 					_ => {
-						match (left, right) {
-							(&Rex::Identifier{ref id, ref el}, &Rex::Literal(_)) => {
-								match op {
-									&Operator::EQ => {},
-									_ => return Err(format!("Operator {:?} not supported for encrypted column {}", op, el.name))
+						if let Some((element, literal)) = match (left, right) {
+							(&Rex::Identifier{ref el, ..}, &Rex::Literal(ref l))
+							| (&Rex::Literal(ref l), &Rex::Identifier{ref el, ..}) => {
+								match el.encryption {
+									EncryptionType::NA => None,
+									_ => Some((el, l))
 								}
 							},
-							(&Rex::Literal(_), &Rex::Identifier{..}) => {},
-							_ => {} // Dont care
+							_ => None
+						} {
+							match element.encryption {
+								EncryptionType::NA => {},
+								_ => {
+									match op {
+										&Operator::EQ => {
+											match literal {
+												&LiteralExpr::LiteralLong(ref i, ref val) => {
+													self.valuemap.insert(i.clone(), val.encrypt(&element.encryption));
+												},
+												&LiteralExpr::LiteralString(ref i, ref val) => {
+													self.valuemap.insert(i.clone(), val.clone().encrypt(&element.encryption));
+												}
+												_ => panic!("Unsupported value type {:?}", literal)
+											}
+										},
+										_ => return Err(format!("Operator {:?} not supported for encrypted column {}", op, element.name))
+									}
+								}
+							}
+
 						}
 					} // TBD
 				}
