ae46620c0d74a5097ad7401751bdbf735f2bd3cf
Kenji Hollis <kenji.hollis@codefutures.com>
2016-09-02 11:51:35 -0600

    Updated tests to create/drop databases.

--- scripts/jenkins/test.sh
+++ scripts/jenkins/test.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 #
 # Script for testing AgilData Zero with Jenkins
+#
+# For the record, this script is very chatty.  This is so we can verify
+# each step of the way as the build runs.
+
+RUST_BUILD="nightly-2016-08-03"
+AGILDATA_TEST_DB="itest-`date +%s%N`"
 
 # Set the version of Rust to use
-rustup override set nightly-2016-08-03
+echo "Switching to Rust build: $RUST_BUILD"
+rustup override set $RUST_BUILD
+
+# Clear out binaries already built
+echo "Clearing out already built binaries."
+rm -rf target
 
 # Build Zero
+echo "Building AgilData Zero"
 cargo build
 
 # Start server in the background, storing the PID of the app
@@ -16,15 +28,22 @@ ls -al target/debug
 echo "Launching AgilData Zero."
 target/debug/agildata-zero &
 AGILDATA_ZERO_PID=$!
+echo "AgilData Zero launched: Process ID=$AGILDATA_ZERO_PID"
 
 # PS to make sure the process is running
-ps -ax | grep $AGILDATA_ZERO_PID
+ps -aux | grep $AGILDATA_ZERO_PID | grep -v grep
 
-# Drop test databases
+# Create Database
+echo "Creating database: $AGILDATA_TEST_DB"
+mysql --host=127.0.0.1 --port=3307 -e "CREATE DATABASE $AGILDATA_TEST_DB CHARACTER SET UTF8"
 
 # Copy test database info into MySQL
 # Query MySQL and run diffs against the values
 # If any diffs occur, indicate a build error, and report back the differences
+
+# Drop Database
+echo "Dropping database: $AGILDATA_TEST_DB"
+mysql --host=127.0.0.1 --port=3307 -e "DROP DATABASE $AGILDATA_TEST_DB"
 
 # Stop AgilData Zero
 echo "Stopping AgilData Zero: $AGILDATA_ZERO_PID"
