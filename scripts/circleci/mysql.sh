af8deee1bc9a728151a3bf352cdc1663d9f6863d
Kenji Hollis <kenji.hollis@codefutures.com>
2016-09-12 16:51:28 -0600

    Updated to include corrected mysql startup.

--- scripts/circleci/mysql.sh
+++ scripts/circleci/mysql.sh
@@ -8,12 +8,10 @@ MYSQL_PASS="password123"
 MYSQL_PORT="3306"
 MYSQL_VERSION="5.7.12"
 
-docker run
-    --detach
-    --name mysql
-    --publish $MYSQL_PORT:$MYSQL_PORT
-    --env MYSQL_ALLOW_EMPTY_PASSWORD='yes'
-    --env MYSQL_DATABASE=$MYSQL_DATABASE
-    --env MYSQL_USER=$MYSQL_USER
-    --env MYSQL_PASSWORD=$MYSQL_PASS
-    mysql:$MYSQL_VERSION ; sleep 10
+echo
+echo "Starting Docker for MySQL version $MYSQL_VERSION"
+docker run --detach --name mysql --publish $MYSQL_PORT:$MYSQL_PORT --env MYSQL_ALLOW_EMPTY_PASSWORD='yes' --env MYSQL_DATABASE=$MYSQL_DATABASE --env MYSQL_USER=$MYSQL_USER --env MYSQL_PASSWORD=$MYSQL_PASS mysql:$MYSQL_VERSION
+
+echo
+echo "Sleeping 10 seconds to allow MySQL docker image to stabilize."
+sleep 10
