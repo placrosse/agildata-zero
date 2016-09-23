77884ce637aa59fa9d248a866d4d0edaa3401294
Drew Manlove <andrew.manlove@codefutures.com>
2016-09-23 08:35:52 -0600

    hash out fledgling statement cache api

--- src/proxy/mod.rs
+++ src/proxy/mod.rs
@@ -3,3 +3,4 @@ pub mod server;
 pub mod encrypt_visitor;
 mod writers;
 pub mod schema_provider;
+pub mod statement_cache;
---
+++ src/proxy/statement_cache.rs
@@ -0,0 +1,52 @@
+use std::sync::Mutex;
+use std::rc::Rc;
+use std::collections::HashMap;
+use encrypt::{NativeType, EncryptionType};
+
+#[derive(Debug, PartialEq)]
+pub enum ValueType {
+    BOUND_PARAM(u32),
+    LITERAL(u32),
+    COLUMN
+}
+
+#[derive(Debug, PartialEq)]
+pub struct EncryptionPlan {
+    data_type: NativeType,
+    encryption: EncryptionType,
+    value_type: ValueType
+}
+
+#[derive(Debug, PartialEq)]
+pub struct PhysicalPlan {
+    literals: Vec<EncryptionPlan>,
+    params: Vec<EncryptionPlan>,
+    result: Vec<EncryptionPlan>
+}
+
+struct StatementCache {
+    cache: Mutex<HashMap<u64, Rc<PhysicalPlan>>>
+}
+
+impl StatementCache {
+    pub fn new() -> Self {
+        StatementCache {
+            cache: Mutex::new(HashMap::new())
+        }
+    }
+
+    pub fn get(&self, key: &u64) -> Option<Rc<PhysicalPlan>> {
+        let data = self.cache.lock().unwrap();
+
+        match data.get(key) {
+            Some(rc) => Some(rc.clone()),
+            None => None
+        }
+    }
+
+    pub fn put(&self, key: u64, ep: PhysicalPlan) {
+        let mut data = self.cache.lock().unwrap();
+
+        data.insert(key, Rc::new(ep));
+    }
+}
