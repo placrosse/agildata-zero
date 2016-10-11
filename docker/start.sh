d6bad4cb4682d32f5fd0b0c1dc472f29ba2771ec
JD Schmidt <jd.schmidt@codefutures.com>
2016-10-10 18:06:23 -0600

    fixed another bug in start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -5,9 +5,9 @@ cd /agildata-zero
 
 # this is a hack until agildata-zero can load host from an environmental variable
 MY_IP_ADDRESS="$(hostname -i)"
-sed -i -e "s/127.0.0.1/$MY_IP_ADDRESS/" zero-config.xml
+sed -i -e "0,/127.0.0.1/s/127.0.0.1/$MY_IP_ADDRESS/" zero-config.xml
 if [ -n "$MYSQL_PORT_3306_TCP_ADDR" ]; then
-  sed -i -e "s/127.0.0.1/$MYSQL_PORT_3306_TCP_ADDR/" zero-config.xml
+  sed -i -e "0,/127.0.0.1/s/127.0.0.1/$MYSQL_PORT_3306_TCP_ADDR/" zero-config.xml
 fi
 
 ./agildata-zero --config zero-config.xml --logconfig log.toml
